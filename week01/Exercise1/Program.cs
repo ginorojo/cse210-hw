6fcb1088b4c4577fdba7c7096064ada239352467
ginorojo <ginorojoj@gmail.com>
2025-01-14 12:16:58 -0600

    excercises week 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+
+        Console.Write("Whats is your name? ");
+        string name= Console.ReadLine();
+        Console.Write("whats is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"your name is {name} {lastName}");
+    
+
+     
+
+
+    }  
 }
