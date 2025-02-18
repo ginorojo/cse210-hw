40cdcb0e47aaca93e1421424a9fa083c9a6a2fe8
ginorojo <ginorojoj@gmail.com>
2025-02-18 11:05:17 -0300

    assigment week 7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("18 Feb 2025", 30, 4.8),
+            new Cycling("18 Feb 2025", 45, 20.0),
+            new Swimming("18 Feb 2025", 25, 40)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
